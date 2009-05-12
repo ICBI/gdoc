c8610ab4b0061af0ebe82dc09fddeebfb7e8ac1c
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2009-05-12 22:54:37 +0000

    adding share method
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@128 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/services/LoginService.groovy
+++ grails-app/services/LoginService.groovy
@@ -2,16 +2,20 @@ import gov.nih.nci.security.SecurityServiceProvider;
 import gov.nih.nci.security.AuthenticationManager;
 import gov.nih.nci.security.AuthorizationManager;
 import gov.nih.nci.security.exceptions.CSException
+import gov.nih.nci.security.authorization.domainobjects.ProtectionElement
+
 import LoginException
 
 class LoginService{
 	
+	AuthenticationManager authenticationManager
+	AuthorizationManager authorizationManager
+	
 	def login(params) throws LoginException{
 		def user = GDOCUser.findByLoginName(params.loginName)
 		try{
-			AuthenticationManager authenticationManager = SecurityServiceProvider.getAuthenticationManager("gdoc");
 			//print authenticationManager.getApplicationContextName();
-			boolean loginOK = authenticationManager.login(params.loginName, params.password);
+			boolean loginOK = this.getAuthenticationManager().login(params.loginName, params.password);
 			if (loginOK){
 				System.out.println("SUCESSFUL LOGIN");
 			}
@@ -26,8 +30,7 @@ class LoginService{
 		}
 		
 		try {
-			AuthorizationManager authorizationManager = SecurityServiceProvider.getAuthorizationManager("gdoc");
-			print "got user " + authorizationManager + " from csm, ready to authorize "
+			print "got user " + this.getAuthorizationManager() + " from csm, ready to authorize "
 			Set memberships = user.getGroups();
 			memberships.each{ membership ->
 				println membership.group.groupName
@@ -46,4 +49,34 @@ class LoginService{
 		session.invalidate()
 	}
 	
+	/**
+	* Share an item with a collaboration groups
+	*/
+	def share(item, itemType, group) {
+		def authManager = this.getAuthorizationManager()
+		
+		ProtectionElement pe = authManager.getProtectionElement(item.id.toString(), itemType)
+		if(!pe) {
+			pe = new ProtectionElement()
+			pe.attribute = itemType
+			pe.objectId = item.id.toString()
+			authManager.createProtectionElement(pe)
+		}
+		
+		authManager.assignProtectionElement(group, item.id.toString(), itemType)
+	}
+	
+	public AuthenticationManager getAuthenticationManager() {
+		if(!this.@authenticationManager) {
+			this.@authenticationManager = SecurityServiceProvider.getAuthenticationManager("gdoc")
+		} 
+		return this.@authenticationManager 
+	}
+	
+	public AuthorizationManager getAuthorizationManager() {
+		if(!this.@authorizationManager) {
+			this.@authorizationManager = SecurityServiceProvider.getAuthorizationManager("gdoc");
+		} 
+		return this.@authorizationManager 
+	}
 }
