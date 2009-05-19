6f5a4f61caa58968378579acb8fb1ec07c7025d2
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2009-05-19 00:48:20 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@135 7e94895d-eddb-4489-9ea7-1067dfd99f16

---
+++ grails-app/utils/Constants.groovy
@@ -0,0 +1,5 @@
+class Constants{
+	def static final USER_LIST = "USER_LIST"
+	def static final SAVED_ANALYSIS = "SAVED_ANALYSIS"
+	
+}
--- grails-app/utils/StudyContextInterceptor.groovy
+++ grails-app/utils/StudyContextInterceptor.groovy
@@ -3,7 +3,7 @@ import org.codehaus.groovy.grails.orm.hibernate.support.ClosureEventTriggeringIn
 class StudyContextInterceptor extends ClosureEventTriggeringInterceptor {
 	String onPrepareStatement(String sql) {
 	 	String prepedStatement = super.onPrepareStatement(sql);
-	println "SQL: ${prepedStatement} STUDY: ${StudyContext.getStudy()}"
+	//println "SQL: ${prepedStatement} STUDY: ${StudyContext.getStudy()}"
 		prepedStatement = prepedStatement.replaceAll("__STUDY_SCHEMA__", StudyContext.getStudy());
 		return prepedStatement;
 	}
