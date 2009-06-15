ba425ea9355856d42ef07b7a2656fc5fde111626
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2009-06-15 22:43:39 +0000

    fixed bug with KM
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@179 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/services/PatientService.groovy
+++ grails-app/services/PatientService.groovy
@@ -1,16 +1,36 @@
 class PatientService {
-
+	static PAGE_SIZE = 1000
     boolean transactional = true
 
     def patientsForGdocIds(gdocIds) {
-		def c = Patient.createCriteria()
 		println "GDOC: $gdocIds"
+		def allPatients
 		gdocIds = gdocIds.collect {
 			it.toLong()
 		}
-		return c.listDistinct {
-			'in'("gdocId", gdocIds)
+		def patients = []
+		def index = 0;
+		while(index < gdocIds.size()) {
+			def c = Patient.createCriteria()
+			def patientsLeft = gdocIds.size() - index
+			def tempPatients
+			if(patientsLeft > PAGE_SIZE) {
+				def tempIds = (gdocIds.getAt(index..<(index + PAGE_SIZE)))
+				tempPatients = c.listDistinct {
+					'in'("gdocId", tempIds)
+				}
+				patients.addAll(tempPatients)
+				index += PAGE_SIZE
+			} else {
+				def tempIds = (gdocIds.getAt(index..<gdocIds.size()))
+				tempPatients = c.listDistinct {
+					'in'("gdocId", tempIds)
+				}
+				patients.addAll(tempPatients)
+				index += patientsLeft
+			}
 		}
+		return patients
 		
     }
 }
