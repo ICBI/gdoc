008a66c14bf66dc0906088c92b7cd2d330085c6e
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-03-11 15:10:52 +0000

    using local plugin repository
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@513 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,3 +5,4 @@ grails.war.resources = {stagingDir ->
 	delete(file: "$stagingDir/WEB-INF/lib/javax.jms.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/commons-collections-2.1.1.jar")
 }
+grails.plugin.repos.discovery.myRepository="https://gallifrey.arc.georgetown.edu/svn/gdoc/grails-plugins/"
