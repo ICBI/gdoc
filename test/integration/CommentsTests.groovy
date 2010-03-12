5133fb4b8fa5da311573514d7dce76c49d56952a
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-03-12 14:03:19 +0000

    fixing tests
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@531 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- test/integration/CommentsTests.groovy
+++ test/integration/CommentsTests.groovy
@@ -1,31 +1,28 @@
-class CommentsTests extends GroovyTestCase {
+class CommentsTests extends BaseIntegrationTest {
 
     void testComment() {
-	def appUser = new GDOCUser(username:'studyUser').save();
-	def appCommentUser = new GDOCUser(username:'studyCommentUser').save();
-	def intGeneList = new UserList(name:'studyGeneList',author:appUser).save();
-	intGeneList.addToListItems(new UserListItem(value:'ER2'))
-		intGeneList.addToListItems(new UserListItem(value:'bR2'))
-			intGeneList.addToListItems(new UserListItem(value:'vfrR'))
-	UserList.findByAuthor(appUser).each{
-		println it.name
-		it.listItems.each{
-			println it.value
+		def appUser = GDOCUser.findByLoginName('acs224')
+		def appCommentUser = GDOCUser.findByLoginName('kmr75')
+		def intGeneList = new UserList(name:'studyGeneList',author:appUser)
+		saveObject(intGeneList)
+		intGeneList.addToListItems(new UserListItem(value:'ER2'))
+			intGeneList.addToListItems(new UserListItem(value:'bR2'))
+				intGeneList.addToListItems(new UserListItem(value:'vfrR'))
+		UserList.findByAuthor(appUser).each{
+			println it.name
+			it.listItems.each{
+				println it.value
+			}
 		}
-	}
-	def commentText = "Wow, that is an interesting list"
-	def comment = new Comments(user:appCommentUser, commentText:commentText,list:intGeneList)
-	intGeneList.addToListComments(comment);
-	appCommentUser.addToComments(comment);
-	def recComment = Comments.findByUser(appCommentUser);
-	recComment.each{
-		println it.commentText
-		println it.user
-		println it.list	
-	}
-	def recList = UserList.find(intGeneList)
-	println recList.listComments
-	def usersList = GDOCUser.find(appCommentUser)
-	println usersList.comments
+		def commentText = "Wow, that is an interesting list"
+		def comment = new Comments(user:appCommentUser, commentText:commentText,list:intGeneList)
+		intGeneList.addToListComments(comment);
+		appCommentUser.addToComments(comment);
+		def recComment = Comments.findByUser(appCommentUser);
+		recComment.each{
+			println it.commentText
+			println it.user
+			println it.list	
+		}
     }
 }
