0748b41db129ba71e4bc5105ec4010835bc029f7
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-03-15 19:17:05 +0000

    firefox issue
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@551 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/SeleniumConfig.groovy
+++ grails-app/conf/SeleniumConfig.groovy
@@ -12,5 +12,5 @@ selenium {
 	screenshotDir = "./target/screenshots"		// directory where screenshots are placed relative to project root
 */
 	port = 4447
-	browser = "*custom /usr/bin/firefox --display=:2"
+	browser = "*custom /home/jboss/startFirefox.sh"
 }
