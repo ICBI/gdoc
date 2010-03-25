c433f77116f9e661a1309fd5917438b62003fa45
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-03-25 17:43:38 +0000

    fixing scripts
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@573 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- sql/02_study_setup_template.sql
+++ sql/02_study_setup_template.sql
@@ -44,11 +44,14 @@ GRANT REFERENCES ON COMMON.PROTOCOL to ${projectName};
 GRANT REFERENCES ON COMMON.ATTRIBUTE_TYPE to ${projectName};
 GRANT REFERENCES ON COMMON.ATTRIBUTE_VOCABULARY to ${projectName};
 GRANT REFERENCES ON COMMON.PATIENT_DATA_SOURCE to ${projectName};
-GRANT REFERENCES ON COMMON.REPORTER_LIST to ${projectName};
+GRANT REFERENCES ON COMMON.HTARRAY_REPORTER_LIST to ${projectName};
 GRANT REFERENCES ON COMMON.FILE_FORMAT to ${projectName};
 GRANT REFERENCES ON COMMON.FILE_TYPE to ${projectName};
-GRANT REFERENCES ON COMMON.mARRAY_REPORTER to ${projectName};
-GRANT REFERENCES ON COMMON.mARRAY_DESIGN to ${projectName};
+GRANT REFERENCES ON COMMON.HTARRAY_REPORTER to ${projectName};
+GRANT REFERENCES ON COMMON.HTARRAY_DESIGN to ${projectName};
+GRANT REFERENCES ON COMMON.MS_PEAK_EVIDENCE to ${projectName};
+GRANT REFERENCES ON COMMON.MS_PEAK_GROUP to ${projectName};
+
 GRANT ALL ON COMMON.patient_data_source to ${projectName} with grant option;
 GRANT ALL ON COMMON.attribute_type to ${projectName} with grant option;
 GRANT ALL ON COMMON.attribute_vocabulary to ${projectName} with grant option;
@@ -58,16 +61,14 @@ GRANT SELECT ON COMMON.ATTRIBUTE_TIMEPOINT_SEQUENCE to ${projectName} with grant
 GRANT SELECT ON COMMON.PATIENT_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.DATA_SOURCE_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.CONTACT_SEQUENCE to ${projectName} with grant option;
-GRANT SELECT ON COMMON.ARRAY_DESIGN_SEQUENCE to ${projectName} with grant option;
-GRANT SELECT ON COMMON.MARRAY_REPORTER_SEQUENCE to ${projectName} with grant option;
-GRANT SELECT ON COMMON.REPORTER_LIST_SEQUENCE to ${projectName} with grant option;
+GRANT SELECT ON COMMON.HTARRAY_DESIGN_SEQUENCE to ${projectName} with grant option;
+GRANT SELECT ON COMMON.HTARRAY_REPORTER_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.BIOSPECIMEN_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.DATA_FILE_SEQUENCE to ${projectName} with grant option;
-GRANT SELECT ON COMMON.MARRAY_RUN_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.PROTOCOL_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.FILE_TYPE_SEQUENCE to ${projectName} with grant option;
 GRANT SELECT ON COMMON.FILE_FORMAT_SEQUENCE to ${projectName} with grant option;
-GRANT SELECT ON COMMON.MARRAY_RUN_BIOSPEC_SEQUENCE to ${projectName} with grant option;
+GRANT SELECT ON COMMON.ALL_DESIGNS to ${projectName} with grant option;
 
 -- not sure why i need this here....would it not be covered by read_common
 grant select on common.attribute_type to ${projectName};
