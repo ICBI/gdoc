8bd17a9fe39a67ee3eb9b244e3e84c4889a9a729
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-06-13 12:27:03 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@699 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- src/java/serviceinterfaces/SessionCleanerServiceInt.java
+++ src/java/serviceinterfaces/SessionCleanerServiceInt.java
@@ -1,6 +1,8 @@
 package serviceinterfaces;
 import java.util.HashSet;
 
+/** interface responsible for cleaning session data **/
+
 public interface SessionCleanerServiceInt { 
 	
 	public void cleanup(String userId, HashSet lists, HashSet analyses);
