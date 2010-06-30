12d4e885c21775de3335765160328cacf35c9cdb
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-06-30 18:04:09 +0000

    adding validation changes
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@738 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/controllers/GenomeBrowserCommand.groovy
+++ grails-app/controllers/GenomeBrowserCommand.groovy
@@ -13,20 +13,24 @@ class GenomeBrowserCommand {
 	static constraints = {
 		searchType(blank:false)
 		feature(validator: { val, obj ->
-			if(obj.searchType == 'feature')
-				return (val != '')
+			if((obj.searchType == 'feature') && (val == ''))
+				return "custom.blank"
 			return true
 		})
 		location(validator: { val, obj -> 
-			if(obj.searchType == 'location')
-				return (val != '')
+			if((obj.searchType == 'location')) {
+				if(val == '')
+					return "custom.blank"
+				if(!val.isLong())
+					return "custom.long"
+			}
 			return true
 		})
 		omicsTypes(validator: { val, obj -> 
 			if(obj.omicsData) {
 				def session = RCH.requestAttributes.session
 				if(!session.study)
-					return false
+					return "custom.study"
 			}
 		})
 	}
