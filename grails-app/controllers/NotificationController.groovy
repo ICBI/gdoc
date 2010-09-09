d38e5bce5a718a4a10959b46bbdf4d64a4ef60a2
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-09-09 18:15:12 +0000

    cleaning up
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@1038 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/controllers/NotificationController.groovy
+++ grails-app/controllers/NotificationController.groovy
@@ -19,21 +19,23 @@ class NotificationController {
 	def checkNotifications = {
 		def analysis = savedAnalysisService.checkSavedAnalysis(session.userId)
 		def statusList = []
+		def today = new Date()
 		analysis.each{
 			def statusMap = [:]
-			statusMap["id"] = it[0]
-			statusMap["type"] = it[1]
-			statusMap["status"] = it[2]
-			statusMap["dateCreated"] = it[3]
-			statusList << statusMap
+			if(today.minus(it[3]) < 2){
+				statusMap["id"] = it[0]
+				statusMap["type"] = it[1]
+				statusMap["status"] = it[2]
+				statusMap["dateCreated"] = it[3]
+				statusList << statusMap
+			}
 		}
 		session.notifications = statusList.sort { it.dateCreated }.reverse()
 	}
 	
 	def delete = {
 		savedAnalysisService.deleteAnalysis(params.id)
-		checkNotifications()
-		render(template:"/notification/notificationTable")
+		redirect(action:index)
 	}
 	
 	def error = {
@@ -43,34 +45,6 @@ class NotificationController {
 			def words = wrapntab(errorMessage)
 			render words.join("<br/>")
 		}
-	}
-	def buildNotifications = {
-		def notifications = []
-		def savedAnalysis = []
-		def invitations = []
-		savedAnalysis = savedAnalysisService.getAllSavedAnalysis(session.userId)
-		if(savedAnalysis){
-			notifications.addAll(savedAnalysis)
-		}
-		
-		//remove analysis that are more than 2 days old
-		def today = new Date()
-		def removeable = []
-		notifications.each{ n ->
-			//log.debug today.minus(n.dateCreated)
-			if(today.minus(n.dateCreated) > 2){
-				//log.debug "remove " + n
-				removeable << n
-			}
-		}
-		notifications.removeAll(removeable)
-		
-		notifications = notifications.sort { one, two ->
-			def dateOne = one.dateCreated
-			def dateTwo = two.dateCreated
-			return dateTwo.compareTo(dateOne)
-		}
-		session.notifications = notifications
 	}
 	
 	private def wrapntab(input, linewidth = 50, indent = 0) throws IllegalArgumentException {
--- grails-app/views/notification/_savedAnalysisItem.gsp
+++ grails-app/views/notification/_savedAnalysisItem.gsp
@@ -11,15 +11,6 @@
 		</g:elseif>		
 		<g:elseif test="${notification.type == AnalysisType.HEATMAP}">
 			<g:link controller="heatMap" action="view" id="${notification.id}">${notification.type}</g:link>
-			<%--APPLET CODE="edu/stanford/genetics/treeview/applet/ButtonApplet.class"
-			  archive="/gdoc/applets/treeview/TreeViewApplet.jar,/gdoc/applets/treeview/nanoxml-2.2.2.jar,/gdoc/applets/treeview/Dendrogram.jar" width=75 height=20
-			    alt="Your browser understands the &lt;APPLET&gt; tag but isn't running the applet, for some reason.">
-			    Your browser is completely ignoring the &lt;APPLET&gt; tag!
-			    <param name="cdtFile" value="${grailsApplication.config.grails.serverURL}/gdoc/heatMap/file?id=${notification.id}&name=cluster.cdt">
-			    <param name="cdtName" value="${grailsApplication.config.grails.serverURL}/gdoc/heatMap/file?id=${notification.id}name=cluster">
-				<param name="plugins" value="edu.stanford.genetics.treeview.plugin.dendroview.DendrogramFactory">
-			  </applet--%>
-			
 			 (<g:formatDate date="${notification.dateCreated}" format="h:mm M/dd/yyyy"/> ) 
 		</g:elseif>		
 		<g:else>
