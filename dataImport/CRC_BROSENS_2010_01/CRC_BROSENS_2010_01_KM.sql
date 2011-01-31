a6abc04564aba73280604809bf5bf70741f8f3b7
kb472 <kb472@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-01-31 17:24:00 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@1353 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- dataImport/CRC_BROSENS_2010_01/CRC_BROSENS_2010_01_KM.sql
+++ dataImport/CRC_BROSENS_2010_01/CRC_BROSENS_2010_01_KM.sql
@@ -16,10 +16,3 @@ values (
 (select attribute_type_id from common.attribute_type where short_name = 'EVENT_OS'), 
 (select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'EVENT_OS' and v.term_meaning = 'EVENT') 
 );
-
-insert into CRC_BROSENS_2010_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
-values ( 
-(select attribute_type_id from common.attribute_type where short_name = 'DISEASE_SPECIFIC_SURVIVAL_MON'), 
-(select attribute_type_id from common.attribute_type where short_name = 'EVENT_DSS'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'EVENT_DSS' and v.term_meaning = 'EVENT') 
-);
