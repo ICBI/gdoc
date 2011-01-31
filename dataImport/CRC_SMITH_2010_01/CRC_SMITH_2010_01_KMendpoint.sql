ec430b775b91cfa71c24a67aa2915c794baf90ba
kb472 <kb472@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-01-31 16:41:34 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/trunk@1351 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- dataImport/CRC_SMITH_2010_01/CRC_SMITH_2010_01_KMendpoint.sql
+++ dataImport/CRC_SMITH_2010_01/CRC_SMITH_2010_01_KMendpoint.sql
@@ -24,5 +24,5 @@ insert into CRC_SMITH_2010_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID
 values ( 
 (select attribute_type_id from common.attribute_type where short_name = 'DISEASE_SPECIFIC_SURVIVAL_MON'), 
 (select attribute_type_id from common.attribute_type where short_name = 'EVENT_DSS'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'EVENT_DFS' and v.term_meaning = 'EVENT') 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'EVENT_DSS' and v.term_meaning = 'EVENT') 
 );
