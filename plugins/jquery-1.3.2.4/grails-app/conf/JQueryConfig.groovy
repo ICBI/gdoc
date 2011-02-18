286284922ae16a12ad1a3a81566f53f560bce5b5
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-02-18 14:04:32 +0000

    refactoring to use grails 1.3.6
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore_refactor@1407 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- plugins/jquery-1.3.2.4/grails-app/conf/JQueryConfig.groovy
+++
@@ -1,4 +0,0 @@
-jquery {
-    sources = 'jquery' // Holds the value where to store jQuery-js files /web-app/js/
-    version = '1.3.2' // The jQuery version in use
-}
--- plugins/jquery-1.3.2.4/src/groovy/org/codehaus/groovy/grails/plugins/jquery/JQuery.groovy
+++
@@ -1,7 +0,0 @@
-package org.codehaus.groovy.grails.plugins.jquery
-/**
- *
- * @author craigjones Jul 16, 2008
- */
-class JQuery {
-}
