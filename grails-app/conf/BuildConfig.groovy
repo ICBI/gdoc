3364288243345a2d38448ac7de6ba8a3a7033867
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-03-22 20:54:21 +0000

    adding heatmap as plugin
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore_refactor@1525 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,3 +9,4 @@ grails.war.resources = {stagingDir ->
 }
 grails.plugin.location.'gcore' = "../gcore"
 grails.plugin.location.'group-comparison' = "../group-comparison"
+grails.plugin.location.'heatmap' = "../heatmap"
