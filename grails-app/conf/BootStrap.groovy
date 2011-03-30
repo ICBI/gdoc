34e7ce14599c840fb89a8493432cee6b2a49f00e
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-03-30 13:11:57 +0000

    added expressionLookup marshaller and initilaize in bottstrap
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore_refactor@1594 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,6 +6,8 @@ import org.apache.commons.lang.StringUtils
 
 class BootStrap {
      def init = { servletContext ->
+		// Initialize custom json converter
+		grails.converters.JSON.registerObjectMarshaller(new ExpressionLookupResultMarshaller())
      }
      def destroy = {
      }
---
+++ grails-app/utils/ExpressionLookupResultMarshaller.groovy
@@ -0,0 +1,41 @@
+import org.codehaus.groovy.grails.web.converters.marshaller.json.DomainClassMarshaller
+import org.codehaus.groovy.grails.web.converters.exceptions.ConverterException
+import org.codehaus.groovy.grails.commons.GrailsDomainClassProperty
+import org.codehaus.groovy.grails.commons.GrailsDomainClass
+import grails.converters.*
+import gov.nih.nci.caintegrator.analysis.messaging.ExpressionLookupResult
+
+public class ExpressionLookupResultMarshaller extends DomainClassMarshaller {
+
+	public boolean supports(Object object) {
+		return (object instanceof ExpressionLookupResult)
+	}
+	
+	public void marshalObject(Object value, JSON json) {
+		/*
+		json.build{
+			taskId(value.taskId)
+			dataVectors {
+				value.dataVectors.each {
+					name(it.name)
+					dataPoints(it.dataPoints)
+				}
+			}
+		}
+		*/
+		println "start marshalling data"
+		def item = value
+		def result = [:]
+		def items = []
+		result["taskId"] = item.taskId
+		item.dataVectors.each { 
+			def vector = [:]
+			vector["name"] = it.name
+			vector["dataPoints"] = it.dataPoints
+			items << vector
+		}
+		result["dataVectors"] = items
+		println "end marshalling data " + result
+		json.value(result)
+	}
+}
