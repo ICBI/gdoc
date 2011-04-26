5b283ae3d1dbcf3fa3711d90e1d7e43567e287a8
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-04-26 14:28:59 +0000

    adding resolution
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/webapp/branches/gcore_refactor@1831 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,20 +1,40 @@
 // war file setup
-grails.war.destFile = "gdoc.war"
+grails.project.war.file = "target/${appName}.war"
 grails.war.resources = {stagingDir ->
 	delete(file: "$stagingDir/WEB-INF/lib/jbossall-client.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/javax.jms.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/commons-collections-2.1.1.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/jboss-jca.jar")
 	delete(file: "$stagingDir/WEB-INF/lib/ojdbc14.jar")
+	delete(file: "$stagingDir/WEB-INF/lib/geronimo-jms_1.1_spec-1.1.1.jar")
+	delete(file: "$stagingDir/WEB-INF/lib/geronimo-spec-jta-1.0.1B-rc4.jar")
 }
-grails.plugin.location.'gcore' = "../gcore"
-grails.plugin.location.'group-comparison' = "../group-comparison"
-grails.plugin.location.'heatmap' = "../heatmap"
-grails.plugin.location.'cin' = "../cin"
-grails.plugin.location.'gene-expression-search' = "../gene-expression-search"
-grails.plugin.location.'genome-browser' = "../genome-browser"
-grails.plugin.location.'pca' = "../pca"
-grails.plugin.location.'km' = "../km"
-grails.plugin.location.'cytoscape' = "../cytoscape"
-grails.plugin.location.'molecule-target' = "../molecule-target"
-grails.plugin.location.'quick-start' = "../quick-start"
+
+javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+new javax.net.ssl.HostnameVerifier(){
+
+    public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+            return true;
+    }
+});
+grails.project.dependency.resolution = {
+	inherits("global") {
+	        // uncomment to disable ehcache
+	        // excludes 'ehcache'
+	}
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsRepo "https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gcore-plugins"
+		grailsCentral()
+	}
+}
+//grails.plugin.location.'gcore' = "../gcore"
+//grails.plugin.location.'group-comparison' = "../group-comparison"
+//grails.plugin.location.'heatmap' = "../heatmap"
+//grails.plugin.location.'cin' = "../cin"
+//grails.plugin.location.'genome-browser' = "../genome-browser"
+//grails.plugin.location.'pca' = "../pca"
+//grails.plugin.location.'km' = "../km"
+//grails.plugin.location.'cytoscape' = "../cytoscape"
+//grails.plugin.location.'molecule-target' = "../molecule-target"
