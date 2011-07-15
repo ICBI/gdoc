d82cbd006383869b1da26a9b1c6917ad827436e3
kb472 <kb472@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-07-15 15:32:38 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gdoc-webapp/trunk@2727 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- dataImport/CRC_PILOT_01/CRC_PILOT_01_KMendpoint.sql
+++ dataImport/CRC_PILOT_01/CRC_PILOT_01_KMendpoint.sql
@@ -1,12 +1,10 @@
 -------------------------------
---indivumed (crc_pilot)
---SURGERY_TO_RECUR/FU_DAYS
+--SURGERY_TO_RECUR/FU_MONTHS
 --RECURRENCE_ANY
 insert into CRC_PILOT_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
 values ( 
-(select attribute_type_id from common.attribute_type where short_name = 'SURGERY_TO_RECUR/FU_MONTHS), 
+(select attribute_type_id from common.attribute_type where short_name = 'SURGERY_TO_RECUR/FU_MONTHS'), 
 (select attribute_type_id from common.attribute_type where short_name = 'RECURRENCE_ANY'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'RECURRENCE_ANY' and v.term_meaning = 'YES') 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'RECURRENCE_ANY' and v.term_meaning = 'YES')
 );
-
 -------------------------------------------
