a463f0f07dd484268cbc40d9b3cc5abb872cea2f
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-09-29 19:26:15 +0000

    removing extra files
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gdoc-webapp/trunk@3204 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- web-app/js/dojo/nls/jbrowse_dojo_sl.js
+++
@@ -1,1 +0,0 @@
-dojo.provide("dojo.nls.jbrowse_dojo_sl");dojo.provide("dijit.nls.loading");dijit.nls.loading._built=true;dojo.provide("dijit.nls.loading.sl");dijit.nls.loading.sl={"loadingState":"Nalaganje...","errorState":"Oprostite, prišlo je do napake."};
