04841bb0b2b225fa6affec06809bcddcc59056f0
kb472 <kb472@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-10-19 18:47:08 +0000

    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gdoc-webapp/trunk@3235 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- dataImport/OV_TCGA_2011_01/OV_TCGA_2011_01_KMplot.sql
+++ dataImport/OV_TCGA_2011_01/OV_TCGA_2011_01_KMplot.sql
@@ -8,20 +8,11 @@ values (
 );
 
 
---TIME: OVERALL_SURVIVAL_DAYS
---end point: VITAL_STATUS
+--TIME: PFS
+--end point: PRogression=yes
 insert into OV_TCGA_2011_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
 values ( 
 (select attribute_type_id from common.attribute_type where short_name = 'PROGRESSION_FREE_SURVIVAL_DAYS'), 
 (select attribute_type_id from common.attribute_type where short_name = 'PROGRESSION'), 
 (select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'PROGRESSION' and v.term_meaning = 'YES') 
-);
-
---TIME: OVERALL_SURVIVAL_DAYS
---end point: VITAL_STATUS
-insert into OV_TCGA_2011_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
-values ( 
-(select attribute_type_id from common.attribute_type where short_name = 'PLATINUM_FREE_SURVIVAL_MONTHS'), 
-(select attribute_type_id from common.attribute_type where short_name = 'PLATINUM_STATUS'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'PLATINUM_STATUS' and v.term_meaning = 'RESISTANT') 
 );
