59195c7513a267e124838a93d52466d596988a4f
kmr75 <kmr75@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2012-01-18 21:05:29 +0000

    feed service additions
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gdoc-webapp/trunk@3441 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -8,6 +8,8 @@ appLongName="Georgetown Database of Cancer"
 appVersion="1.4.2"
 appLogo="gdocHeader.png"
 appContactEmail="gdoc-help@georgetown.edu"
+grails.feedURL="https://wiki.uis.georgetown.edu/createrssfeed.action?types=blogpost&spaces=informatics&title=UIS+Wiki+RSS+Feed&labelString%3D&excludedSpaceKeys%3D&sort=modified&maxResults=10&timeSpan=5&showContent=true&showDiff=true&confirm=Create+RSS+Feed"
+
 
 environments {
     devserver {
