98d878cf991ab16f564da29ca97c2df39b431d5e
kb472 <kb472@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2012-04-05 15:48:46 +0000

    added KM values for LR and DR
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/gdoc-webapp/trunk@3468 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- dataImport/BRC_CLARKE_LIU_9999_01/BRC_CLARKE_LIU_9999_01_KMendpoint.sql
+++ dataImport/BRC_CLARKE_LIU_9999_01/BRC_CLARKE_LIU_9999_01_KMendpoint.sql
@@ -12,3 +12,17 @@ values (
 (select attribute_type_id from common.attribute_type where short_name = 'RR'), 
 (select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'RR' and v.term_meaning = 'YES') 
 );
+
+insert into BRC_CLARKE_LIU_9999_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+values ( 
+(select attribute_type_id from common.attribute_type where short_name = 'SURGERY_TO_DR/FU'), 
+(select attribute_type_id from common.attribute_type where short_name = 'DR'), 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'DR' and v.term_meaning = 'YES') 
+);
+
+insert into BRC_CLARKE_LIU_9999_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+values ( 
+(select attribute_type_id from common.attribute_type where short_name = 'SURGERY_TO_LR/FU'), 
+(select attribute_type_id from common.attribute_type where short_name = 'LR'), 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'LR' and v.term_meaning = 'YES') 
+);
