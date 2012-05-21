bda0efa0f46e024232c86b3433b4071c65f17ff8
Kevin Rosso <kmr75@georgetown.edu>
2012-05-21 12:04:26 -0400

    increment version

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -5,7 +5,7 @@ grails.json.legacy.builder = false
 
 appTitle="G-DOC &reg;"
 appLongName="Georgetown Database of Cancer"
-appVersion="1.4.2"
+appVersion="1.5"
 appLogo="gdocHeader.png"
 appContactEmail="gdoc-help@georgetown.edu"
 grails.newsFeedURL="https://wiki.uis.georgetown.edu/createrssfeed.action?types=blogpost&spaces=informatics&title=UIS+Wiki+RSS+Feedddd&labelString=news&excludedSpaceKeys%3D&sort=modified&maxResults=10&timeSpan=365&confirm=Create+RSS+Feed&showContent=false&showDiff=false"
