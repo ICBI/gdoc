e63647555f4ffd08483d0033e84dbe55c2587fcd
belouali <belouali@gmail.com>
2013-07-29 17:42:13 -0400

    Dicom Viewer Settings

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,6 +45,7 @@ grails.project.dependency.resolution = {
 		compile 'edu.georgetown.gcore:pca:2.0.0'
 		compile 'edu.georgetown.gcore:quick-start:2.0.0' 
 		compile 'edu.georgetown.gcore:next-gen:latest.integration'
+		compile 'edu.georgetown.gcore:dicom-viewer:0.1'
 		compile	':taggable:0.6.1'
 		compile	':spring-security-core:1.2.7.3'
 		compile	':spring-security-ldap:1.0.5'
@@ -61,6 +62,7 @@ grails.project.dependency.resolution = {
 	}
 }
 //grails.plugin.location.'nextgen' = "../grails-nextgen"
+//grails.plugin.location.'dicom-viewer' = "../grails-dicom-viewer"
 //grails.plugin.location.'gcore' = "../grails-gcore"
 //grails.plugin.location.'gene-expression-search' = "../grails-gene-expression-search"
 //grails.plugin.location.'analysis-core' = "../grails-analysis-core"
