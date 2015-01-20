fe84b0b8e28b02e7afe18d009ccc97bceb969222
Krithika <kb472@georgetown.edu>
2015-01-20 11:25:30 -0500

    rembrandt remapping with survival data

---
+++ dataImport/CA_ARRAY_LUNG/CA_ARRAY_LUNG_KMendpoint.sql
@@ -0,0 +1,18 @@
+--CA_ARRAY_LUNG
+--TIME: MONTHS_TO_FIRST_PROGRESSION
+--end point: FIRST_PROGRESSION_OR_RELAPSE
+insert into CA_ARRAY_LUNG.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+values ( 
+(select attribute_type_id from common.attribute_type where short_name = 'MONTHS_TO_FIRST_PROGRESSION'), 
+(select attribute_type_id from common.attribute_type where short_name = 'FIRST_PROGRESSION_OR_RELAPSE'), 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'FIRST_PROGRESSION_OR_RELAPSE' and v.term_meaning = 'YES') 
+);
+
+--TIME: OVERALL_SURVIVAL_MNTHS
+--end point: VITAL_STATUS
+insert into BRC_DESMEDT_2007_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+values ( 
+(select attribute_type_id from common.attribute_type where short_name = 'OVERALL_SURVIVAL_MNTHS'), 
+(select attribute_type_id from common.attribute_type where short_name = 'VITAL_STATUS'), 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'VITAL_STATUS' and v.term_meaning = 'DEAD') 
+);
