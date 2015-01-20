0eb43b534a9e1e8693061597ba9472b7eba404c3
Krithika <kb472@georgetown.edu>
2015-01-20 12:45:41 -0500

    edits

--- dataImport/REMBRANDT/REMBRANDT_KM.sql
+++ dataImport/REMBRANDT/REMBRANDT_KM.sql
@@ -1,6 +1,6 @@
 --TIME: OVERALL_SURVIVAL_MNTHS
 --end point: EVENT_OS
-insert into BRC_DESMEDT_2007_01.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+insert into REMBRANDT.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
 values ( 
 (select attribute_type_id from common.attribute_type where short_name = 'OVERALL_SURVIVAL_MONTHS'), 
 (select attribute_type_id from common.attribute_type where short_name = 'EVENT_OS'), 
