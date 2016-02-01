c2da1a4571f10116c93a3b062caaf80851f185b5
Krithika <kb472@georgetown.edu>
2016-02-01 15:56:15 -0500

    edits

--- dataImport/REMBRANDT_02/REMBRANDT_02_KM.sql
+++ dataImport/REMBRANDT_02/REMBRANDT_02_KM.sql
@@ -4,5 +4,5 @@ insert into REMBRANDT_02.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CEN
 values ( 
 (select attribute_type_id from common.attribute_type where short_name = 'OVERALL_SURVIVAL_DAYS'), 
 (select attribute_type_id from common.attribute_type where short_name = 'CENSORING_STATUS'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'CENSORING_STATUS' and v.term_meaning = '1') 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'CENSORING_STATUS' and v.term_meaning = 'Censoring Status 1') 
 );
