fe9c001fa1e5c3cc85baf2dcc67aee753a6c2959
Krithika <kb472@georgetown.edu>
2016-02-01 15:07:41 -0500

    editin files

--- dataImport/REMBRANDT_02/REMBRANDT_02_KM.sql
+++ dataImport/REMBRANDT_02/REMBRANDT_02_KM.sql
@@ -1,8 +1,8 @@
 --TIME: OVERALL_SURVIVAL_MNTHS
 --end point: EVENT_OS
-insert into REMBRANDT.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
+insert into REMBRANDT_02.km_attribute (KM_ATTRIBUTE_ID, CENSOR_ATTRIBUTE_ID, CENSOR_VALUE_ID) 
 values ( 
-(select attribute_type_id from common.attribute_type where short_name = 'OVERALL_SURVIVAL_MONTHS'), 
-(select attribute_type_id from common.attribute_type where short_name = 'EVENT_OS'), 
-(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'EVENT_OS' and v.term_meaning = 'EVENT') 
+(select attribute_type_id from common.attribute_type where short_name = 'OVERALL_SURVIVAL_DAYS'), 
+(select attribute_type_id from common.attribute_type where short_name = 'CENSORING_STATUS'), 
+(select attribute_vocabulary_id from common.attribute_vocabulary v, common.attribute_type t where t.attribute_type_Id = v.attribute_type_id and t.short_name = 'CENSORING_STATUS' and v.term_meaning = '1') 
 );
